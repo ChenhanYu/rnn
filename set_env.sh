7cdf6e5a5ffaa55e63e6c4613bfcee00e5ffcc88
dialecticDolt <will.ruys@gmail.com>
2021-06-14 15:12:44 -0500

    Added search for BLAS library

--- set_env.sh
+++ set_env.sh
@@ -11,6 +11,7 @@ export GSKNN_ARCH_MINOR=sandybridge
 
 #export GSKNN_ARCH_MAJOR=arm
 #export GSKNN_ARCH_MINOR=neon
+
 export GSKNN_ARCH=$GSKNN_ARCH_MAJOR/$GSKNN_ARCH_MINOR
 echo "GSKNN_ARCH = $GSKNN_ARCH"
 
@@ -19,23 +20,17 @@ export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/opt/intel/lib:/opt/intel/mkl/lib
 echo "DYLD_LIBRARY_PATH = $DYLD_LIBRARY_PATH"
 
 ## Compiler options (if false, then use GNU compilers)
-export GSKNN_USE_INTEL=true
-echo "GSKNN_USE_INTEL = $GSKNN_USE_INTEL"
-export GSKNN_USE_GNU=false
-echo "GSKNN_USE_GNU = $GSKNN_USE_GNU"
+#export GSKNN_USE_INTEL=true
+#echo "GSKNN_USE_INTEL = $GSKNN_USE_INTEL"
+#export GSKNN_USE_GNU=false
+#echo "GSKNN_USE_GNU = $GSKNN_USE_GNU"
 
 ## Whether use BLAS or not?
-export GSKNN_USE_BLAS=false
+export GSKNN_USE_BLAS="True"
 echo "GSKNN_USE_BLAS = $GSKNN_USE_BLAS"
 
-## Manually set the mkl or openblas path
-export GSKNN_MKL_DIR=/opt/intel/mkl
-#export GSKNN_MKL_DIR=$TACC_MKL_DIR
-echo "GSKNN_MKL_DIR = $GSKNN_MKL_DIR"
-
-export GSKNN_OPENBLAS_DIR=
-echo "GSKNN_OPENBLAS_DIR = $GSKNN_OPENBLAS_DIR"
-
+## Manually set the mkl path/hint
+export MKLROOT=""
 
 ## Parallel options
 export KMP_AFFINITY=compact
